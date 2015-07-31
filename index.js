8659ae2a02bf866225f3f00debc77c5b23cd62b7
Felix D <flx.descoteaux@gmail.com>
2015-07-30 22:31:43 -0400

    added ternary

--- index.js
+++ index.js
@@ -4,7 +4,8 @@ var request = require('superagent');
 
 // patch superagent to attach CSRF-token to all requests
 try {
-  var csrf = document.cookie.match(/csrftoken=(.*?)(?:$|;)/)[1];
+  var csrf = document.cookie.match(/csrftoken=(.*?)(?:$|;)/);
+  csrf = csrf ? csrf[1] : null;
   var end = request.Request.prototype.end;
   request.Request.prototype.end = function(fn) {
     this.set('X-CSRFToken', csrf);
